53bba65c8177f2f56b05fb5b04623b2ef5f93d0e
Varun Venkatesh <varunven@uw.edu>
2020-06-15 22:24:49 -0700

    final push

--- Pokemon Image Type Classifcation Run.py
+++ Pokemon Image Type Classifcation Run.py
@@ -13,64 +13,50 @@ from sklearn.metrics import classification_report, accuracy_score
 import matplotlib.pyplot as plt
 import numpy as np
 import os,random, sys, re
+import csv
 
-SHAPE3D = 120
-SHAPEART = 300
-SHAPE = 475
-
-sprites3d = r"C:/Users/dswhi/.vscode/Pokemon Image Creator/MyScraped3DImages"
-spritesart = r"C:/Users/dswhi/.vscode/Pokemon Image Creator/MyScrapedArtImages"
-sprites = r"C:/Users/dswhi/.vscode/Pokemon Image Creator/MyScrapedImages"
-
-curr = sprites 
-# curr = sprites3d
-# curr = spritesart
-currshape = SHAPE
-# currshape = SHAPE3D
-# currshape = SHAPEART
+SHAPE = 120
+curr = "MyScraped3DImages/"
 
 Pokemon = namedtuple('Pokemon', 'name, type1, type2')
 
 def types(path):
     ''' 
-    Reads the csv file to generate a namedtuple Pokemon for each line 
-    returns a dictionary of pokemon no. as key and (name, type1, type2) as value
+    Returns info about Pokemon
     '''
-    file = open(path, 'r').read().splitlines()
     dict = {}
-    file.pop(0)
-    for f in file:
-        info = f.split(',')
-        mon = info[4]
-        dict[mon] = Pokemon(info[0], info[2], info[3])
+    file = open(path, 'r', encoding='utf-8').read().splitlines()
+    for row in file:
+        info = row.split(',')
+        if len(info)>1:
+            mon = info[4]
+            dict[mon] = Pokemon(info[0], info[2], info[3])
     return dict
 
 def get_pokemon(filepath):
-    type_dict = types(r'C:/Users/dswhi/.vscode/Pokemon Image Creator/pokemon_3d_with_types.csv')
-    ''' returns the Pokemon tuple based on file path '''
+    type_dict = types('pokemon_3d_with_types.csv')
     filename = filepath.split('/')[-1]
-    id = ''.join(re.findall(r'\b\d+\b', filename))
+    filename = filename[:-4]
+    id = ''
+    for mon in type_dict:
+        if type_dict[mon].name == filename:
+            print(mon)
+            id = mon
     return type_dict[id]
 
 def load_models():
-    ''' loads the models '''
-    classifier = load_model(curr+"/model/classifier.h5")
+    classifier = load_model(curr+"/model/classifier1.h5")
     print("Loaded classifiers from disk")
     return classifier
 
 def load_image(imagepath):
-    ''' helper to load image '''
-    test_image = image.load_img(imagepath, target_size = (currshape, currshape))
+    test_image = image.load_img(imagepath, target_size = (SHAPE, SHAPE))
     test_image = image.img_to_array(test_image)
     test_image = np.expand_dims(test_image, axis = 0)
     return test_image
 
 def stats(type_dict, classifier, primary = True):
-    ''' 
-    computes manually the true and predicted numpy arrays 
-    then computes the precision, recall, and f-measure statistics
-    '''
-    test = curr+"/test"
+    test = curr+'type1/test'
     
     true_types = []
     pred_types =[]
@@ -86,28 +72,18 @@ def stats(type_dict, classifier, primary = True):
             pred_types.append(pred[0])
     return np.array(true_types), np.array(pred_types)
 
-def run(evaluate = True, predict = True):
-    '''
-    function to perform prediction using models
-    evaluate determines whether to evaluate model through 
-    the Keras evaluation function, computing precision/recall statistics
-    predict will sample a random pokemon image to predict
-    '''
-    
+def run(evaluate = True, predict = True):    
     classifier = load_models()
 
-    # tests
-    test = curr
-    path = test
+    test = curr+'type1/test'
+    path = test + '/' + random.choice([x for x in os.listdir(test) if os.path.isdir(os.path.join(test, x))])
     imagepath = path + '/' + random.choice(os.listdir(path))
-    # load test sets
     test_datagen = ImageDataGenerator(rescale = 1./255)
-    test_set = test_datagen.flow_from_directory(test, target_size = (currshape, currshape), class_mode = 'categorical')
+    test_set = test_datagen.flow_from_directory(test, target_size = (SHAPE, SHAPE), class_mode = 'categorical')
+    
     if predict:
-        # predict random pokemon
         test_image = load_image(imagepath)
         result1 = classifier.predict_classes(test_image)
-        print(result1)
         predicted_type = [typex for typex, index in test_set.class_indices.items() if index == result1[0]][0]
 
         pokemon = get_pokemon(imagepath)
@@ -115,32 +91,21 @@ def run(evaluate = True, predict = True):
         print("The predicted type is " + predicted_type)
         
     if evaluate:
-        # evaluates models
-        print("Primary Type:")
         accuracy = classifier.evaluate_generator(test_set)
-       
-        v_t, v_p = stats(test_set.class_indices, classifier)
-        report = classification_report(v_t, v_p, target_names = list(test_set.class_indices.keys()))
-        print(report)
-        print("Accuracy: ", accuracy_score(v_t, v_p))
-
-        print("\n ------------------------------- \n")
-        print("Evaluation Statistics")
         print("Loss: ", accuracy[0])
         print("Accuracy: ", accuracy[1])
 
     return classifier, test_set
 
-def predict_single(img, classifier, test_set):
-        ''' predicts the type of the pokemon given by "img" '''
-        test_image = load_image(img)
-        result1 = classifier.predict_classes(test_image)
-        predicted_type = [type for type, index in test_set.class_indices.items() if index == result1[0]][0]
-        test_image.show()
-        plt.show()
-        return predicted_type
+def predict_single(pokemon, classifier, test_set):
+    test_image = load_image(pokemon)
+    result1 = classifier.predict_classes(test_image)
+    predicted_type = [type for type, index in test_set.class_indices.items() if index == result1[0]][0]
+    test_image.show()
+    plt.show()
+    return predicted_type
 
 if __name__ == "__main__":
     p = True
     e = True
-    run(evaluate = e, predict = p)
+    run(True, True)
--- Pokemon Image Type Classifcation.py
+++ Pokemon Image Type Classifcation.py
@@ -1,13 +1,3 @@
-import numpy as np
-import tensorflow as tf
-from tensorflow import keras
-import pandas as pd
-import matplotlib.pyplot as plt
-import PIL
-from PIL import Image
-import time
-import os, random, sys
-import scipy
 from keras.models import Sequential
 from keras.layers import Conv2D
 from keras.layers import MaxPooling2D
@@ -18,41 +8,23 @@ from keras.preprocessing import image
 from keras.preprocessing.image import ImageDataGenerator
 from keras.utils import plot_model
 
-SHAPE3D = 120
-SHAPEART = 300
-SHAPE = 475
+import numpy as np
+import os,random, sys
 
-sprites3d = r"C:\Users\dswhi\.vscode\Pokemon Image Creator\MyScraped3DImages"
-spritesart = r"C:\Users\dswhi\.vscode\Pokemon Image Creator\MyScrapedArtImages"
-sprites = r"C:\Users\dswhi\.vscode\Pokemon Image Creator\MyScrapedImages"
+SHAPE = 120
+curr = "MyScraped3DImages/"
 
-curr = sprites 
-# curr = sprites3d
-# curr = spritesart
-currshape = SHAPE
-# currshape = SHAPE3D
-# currshape = SHAPEART
+#120, 3D. Other two are 300 and 475. Both require resizing of the images before running on the model
 
-TF_CPP_MIN_LOG_LEVEL=2
-
-def create_cnn(primary = True):
+def create_cnn():
     ''' 
-    creates the convolutional neural network classifier 
-    returns the classfier for primary or secondary types
+    creates and returns the convolutional neural network classifier 
     '''
-
-    # sequential layers
     classifier = Sequential()
 
-    # add convolutional layers
-    ''' 
-    3 Convolutional Layers, maxpool layer follows 2 Conv2D layers
-    3 Dropout layers to prevent overfitting
-    after flatten, 2 dense layers to return output
-    '''
     classifier.add(Conv2D(64, (3, 3),
                           padding = 'same',
-                          input_shape = (currshape, currshape, 3),
+                          input_shape = (SHAPE, SHAPE, 3),
                           activation = 'relu'))
     classifier.add(MaxPooling2D(pool_size = (2, 2)))
     classifier.add(Dropout(0.25))
@@ -62,39 +34,25 @@ def create_cnn(primary = True):
     classifier.add(MaxPooling2D(pool_size = (2, 2)))
     classifier.add(Dropout(0.25))
 
-    # flatten output and create fully connected layers
     classifier.add(Flatten())
     classifier.add(Dense(256, input_dim = 4, activation = 'relu'))
     classifier.add(Dropout(0.5))
     
-    # one more category for None in secondary type
-    categories = 18 if primary else 19
+    classifier.add(Dense(18, activation = 'softmax'))
 
-    classifier.add(Dense(categories, activation = 'softmax'))
-
-    # returns built CNN
     return classifier
 
-def train(primary = True, save = True, plot_classifier = False):
+def train():
     '''
-    trains the model for primary or secondary types.
-    Supports saving the model and plotting the model.
-    Returns the classifier as well as history object for plotting.
+    trains the model for primary or secondary types then saves it to local folder
     '''
 
-    # get model
-    classifier = create_cnn(primary)
-
-    # batch size
-    BATCH_SIZE = 32
-
-    # number of training epochs
+    classifier = create_cnn()
+    BATCH_SIZE = 64
     EPOCHS = 20
     
-    # uses adam optimizer and crossentropy loss function
-    classifier.compile(optimizer = 'adam',
-                       loss = 'categorical_crossentropy',
-                       metrics = ['accuracy'])
+    # uses categorical because 19 possible types
+    classifier.compile(optimizer = 'adam', loss = 'categorical_crossentropy', metrics = ['accuracy'])
 
     # data augmentation: prevent further overfit by randomly transforming the training images
     train_datagen = ImageDataGenerator(
@@ -105,47 +63,30 @@ def train(primary = True, save = True, plot_classifier = False):
         width_shift_range = 0.1,
         height_shift_range = 0.1,
         horizontal_flip = True,
-        vertical_flip = True)
-    
+        vertical_flip = True)  
     test_datagen = ImageDataGenerator(rescale = 1./255)
 
-    # file path depends on the model
-    train = curr+'/type1/train' if primary else '/type2/train' 
-    test = curr+'/type1/test' if primary else '/type2/test' 
-
+    # file path depends on the primary vs secondary type
+    train = 'type1_sorted/train' 
+    test = 'type1_sorted/test' 
+    train = curr+train
+    test = curr+test
     # retrieve datasets
-    training_set = train_datagen.flow_from_directory(train,
-                                                     target_size = (SHAPE, SHAPE),
-                                                     batch_size = BATCH_SIZE,
-                                                     class_mode = 'categorical')
+    training_set = train_datagen.flow_from_directory(train, target_size = (SHAPE, SHAPE),
+                                                     batch_size = BATCH_SIZE, class_mode = 'categorical')
     
-    test_set = test_datagen.flow_from_directory(test,
-                                                target_size = (SHAPE, SHAPE),
-                                                batch_size = BATCH_SIZE,
-                                                class_mode = 'categorical')
+    test_set = test_datagen.flow_from_directory(test, target_size = (SHAPE, SHAPE),
+                                                batch_size = BATCH_SIZE, class_mode = 'categorical')
 
-    # training
-    history = classifier.fit_generator(training_set,
-                                       #steps_per_epoch = 752,
-                                       #validation_steps = 188,
-                                       epochs = EPOCHS, 
-                                       validation_data = test_set)
+    history = classifier.fit_generator(training_set, epochs = EPOCHS, validation_data = test_set)
 
-    # save the classifier
-    if save:
-        if not os.path.exists(os.path.dirname(curr+"/model")):
-            os.makedirs(os.path.dirname(curr+"/model"))
-        filename = "classifier1" if primary else "classifier2"
-        classifier.save(curr+"/model/" + filename + ".h5")
-        print("Saved model to disk")
-
+    if not os.path.exists(os.path.dirname(curr+"model/")):
+        os.makedirs(os.path.dirname(curr+"model/"))
+    filename = "classifier1"
+    classifier.save(curr+"model/" + filename + ".h5")
+    print("Saved model to disk")
     return classifier, history
 
 if __name__ == "__main__":
-
-    s = True # save model
-    plt = False # plot model layers
-    
-    # build classifier for type 1 and 2
-    _, h = train(primary = True, save = s, plot_classifier = plt)    
-    _, h2 = train(primary = False, save = s, plot_classifier = plt)
+    #type 1
+    _, h = train()
